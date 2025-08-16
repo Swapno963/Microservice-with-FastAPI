1133c1e85e00d4d7ea90bcf1bb173c0bbc0274c5
swapno963 <swapno963@gmail.com>
2025-08-16 11:46:01 +0600

    PyObjectId class ensureing only valid objectids accepted, converts string into proper objectid objects for db operation and made fastapi docs treat it as a string

--- order-service/app/models/order.py
+++ order-service/app/models/order.py
@@ -5,6 +5,24 @@ from app.core.config import settings
 from datetime import datetime
 
 
+class PyObjectId(ObjectId):
+    """Custom ObjectId type for Pydantic models."""
+
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid ObjectId")
+        return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="string")
+
+
 class OrderItem(BaseModel):
     """Model for an item in an order."""
 
