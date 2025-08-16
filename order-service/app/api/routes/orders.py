f70a4a1b4aabda55ed9fe61e248972636eba31ca
swapno963 <swapno963@gmail.com>
2025-08-16 15:50:10 +0600

    Started work on create order

--- order-service/app/api/routes/orders.py
+++ order-service/app/api/routes/orders.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter, Depends, HTTPException, Path, Query, Body, status
 import logging
-
+from app.models.order import Order, OrderCreate, OrderUpdate, OrderResponse
+from motor.motor_asyncio import AsyncIOMotorDatabase
+from app.api.dependencies import get_db, get_current_user
+from typing import List, Optional, Dict, Any
 
 # Configure logger
 logger = logging.getLogger(__name__)
@@ -8,3 +11,21 @@ logger = logging.getLogger(__name__)
 
 # Create router
 router = APIRouter(prefix="/orders", tags=["orders"])
+
+
+@router.post("/", response_model=OrderResponse, status_code=status.HTTP_201_CREATED)
+async def create_order(
+    order: OrderCreate,
+    db: AsyncIOMotorDatabase = Depends(get_db),
+    current_user: Dict[str, Any] = Depends(get_current_user),
+):
+    """
+    Create a new order.
+
+    1. Verify the usesr exists.
+    2. Verify all products exists and prices are correct.
+    3. Check inventory availability for all the products.
+    4. Reserve inventory for all the products.
+    5. Create the order in the pending status
+
+    """
