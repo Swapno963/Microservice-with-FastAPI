b102ae011f9b02e67930b42b2cda83a95e2f2191
swapno963 <swapno963@gmail.com>
2026-01-20 20:25:43 +0600

    Uvicorn is trying to restart it and failed, solved this issue

--- order-service/app/main.py
+++ order-service/app/main.py
@@ -33,4 +33,4 @@ async def health_check():
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("app.main:app", host="0.0.0.0", port=settings.PORT, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=settings.PORT, reload=False)
